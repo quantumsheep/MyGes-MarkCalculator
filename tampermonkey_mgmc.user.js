182056a5f895650c5c66713efa42cfde76677125
QuantumSheep <nathanael.dmc@outlook.fr>
2018-06-24 00:56:24 +0200

    Add average column

--- tampermonkey_mgmc.user.js
+++ tampermonkey_mgmc.user.js
@@ -16,70 +16,95 @@ var inline_src = (<><![CDATA[
     /* jshint esversion: 6 */
 
     (() => {
-    if (document.getElementById("myges-markcalculator") != null) return;
+        if (document.getElementById("myges-markcalculator") != null) return;
 
-    const marksTable = document.getElementById("marksForm:marksWidget:coursesTable_data");
+        const calculate = () => {
+            document.querySelector('#marksForm\\:marksWidget\\:coursesTable_head tr').innerHTML += `<th id="marksForm:marksWidget:coursesTable:j_idt183" class="ui-state-default" role="columnheader" style="width:55px;text-align: center"><span class="ui-column-title">Moyenne</span></th>`
 
-    let totalavg = 0;
-    let avgcount = 0;
-    let rowcount = 0;
-    let ects = 0;
+            const marksTable = document.getElementById("marksForm:marksWidget:coursesTable_data");
 
-    marksTable.childNodes.forEach(elem => {
-        let avg = 0;
-        let markcount = 0;
+            let totalavg = 0;
+            let avgcount = 0;
+            let rowcount = 0;
+            let ects = 0;
 
-        rowcount++;
+            marksTable.childNodes.forEach(elem => {
+                const td = document.createElement('td');
 
-        for (let i = 4; i < elem.childNodes.length; i++) {
-            if (elem.childNodes[i].innerHTML) {
-                avg += parseFloat(elem.childNodes[i].innerHTML.replace(',', '.'));
-                markcount++;
-            }
-        }
+                td.setAttribute('role', 'gridcell');
+                td.style.width = '55px';
+                td.style.textAlign = 'center';
 
-        let coefIsNaN = false;
+                let avg = 0;
+                let markcount = 0;
 
-        if (markcount > 0) {
-            let coef = parseFloat(elem.childNodes[2].innerHTML);
+                rowcount++;
 
-            if(isNaN(coef)) {
-                coef = 1;
-                coefIsNaN = true;
-            }
-
-            avg = (avg * coef) / (coef * markcount);
-            totalavg += avg;
-            avgcount++;
-
-            let thisects = 0;
-
-            if (avg >= 10) {
-                thisects = parseFloat(elem.childNodes[3].innerHTML);
-
-                if(!isNaN(thisects)) {
-                    ects += thisects;
+                for (let i = 4; i < elem.childNodes.length; i++) {
+                    if (elem.childNodes[i].innerHTML) {
+                        avg += parseFloat(elem.childNodes[i].innerHTML.replace(',', '.'));
+                        markcount++;
+                    }
                 }
-            }
 
-            console.log(`Moyenne pour ${elem.childNodes[0].firstChild.innerHTML} : ${avg} (coef ${coefIsNaN ? 'inconnu (remplacé par 1)' : parseInt(elem.childNodes[3].innerHTML)}) ${thisects > 0 ? `(+${thisects} ECTS)` : ''}`);
+                let coefIsNaN = false;
+
+                if (markcount > 0) {
+                    let coef = parseFloat(elem.childNodes[2].innerHTML);
+
+                    if (isNaN(coef)) {
+                        coef = 1;
+                        coefIsNaN = true;
+                    }
+
+                    avg = (avg * coef) / (coef * markcount);
+                    totalavg += avg;
+                    avgcount++;
+
+                    let thisects = 0;
+
+                    if (avg >= 10) {
+                        thisects = parseFloat(elem.childNodes[3].innerHTML);
+
+                        if (!isNaN(thisects)) {
+                            ects += thisects;
+                        }
+                    }
+
+                    console.log(`Moyenne pour ${elem.childNodes[0].firstChild.innerHTML} : ${avg} (coef ${coefIsNaN ? 'inconnu (remplacé par 1)' : parseInt(elem.childNodes[3].innerHTML)}) ${thisects > 0 ? `(+${thisects} ECTS)` : ''}`);
+                    td.innerText = avg;
+                }
+
+                elem.appendChild(td);
+            });
+
+            totalavg = (totalavg / avgcount).toFixed(2);
+
+            console.log(`Moyenne totale: ${totalavg}`);
+            console.log(`ECTS gagnés: ${ects}`);
+
+            marksTable.innerHTML += `
+                <tr data-ri="12" class="ui-widget-content ui-datatable-even ${rowcount % 2 == 0 ? 'odd' : 'even'}-row" id="myges-markcalculator" role="row">
+                    <td role="gridcell" colspan="3" style="width:250px" class="mg_inherit_bg">
+                        <span class="mg_inherit_color" style="font-weight: bold; font-size: 11px"><b>Moyenne Générale</b></span>
+                    </td>
+                    <td role="gridcell" style="width:37px;text-align: center"><b>${ects}</b></td>
+                    <td role="gridcell" colspan="${marksTable.firstElementChild.childElementCount - 4}" style="width:37px;text-align: center"><b>${totalavg}</b></td>
+                </tr>`;
         }
-    });
 
-    totalavg = (totalavg / avgcount).toFixed(2);
+        let actual = document.querySelector('#marksForm\\:j_idt164\\:periodSelect_panel div ul li.ui-state-highlight').innerText;
 
-    console.log(`Moyenne totale: ${totalavg}`);
-    console.log(`ECTS gagnés: ${ects}`);
+        document.querySelectorAll('#marksForm\\:j_idt164\\:periodSelect_panel div ul li').forEach(elem => elem.addEventListener('click', function (e) {
+            if (this.innerText !== actual) {
+                actual = this.innerText;
 
-    marksTable.innerHTML += `
-        <tr data-ri="12" class="ui-widget-content ui-datatable-even ${rowcount % 2 == 0 ? 'odd' : 'even'}-row" id="myges-markcalculator" role="row">
-            <td role="gridcell" colspan="3" style="width:250px" class="mg_inherit_bg">
-                <span class="mg_inherit_color" style="font-weight: bold; font-size: 11px"><b>Moyenne Générale</b></span>
-            </td>
-            <td role="gridcell" style="width:37px;text-align: center"><b>${ects}</b></td>
-            <td role="gridcell" colspan="${marksTable.firstElementChild.childElementCount - 4}" style="width:37px;text-align: center"><b>${totalavg}</b></td>
-        </tr>`;
-})();
+                setTimeout(calculate, 500);
+            }
+        }));
+
+        calculate();
+    })();
 
 /* jshint ignore:start */
 ]]></>).toString();
